546a3814edcd6c3b080cdb19e8b6620a7ff6b980
maindufeu <edher.jimenez@gmail.com>
2021-03-29 21:33:02 +0000

    Update environment_setup.sh

--- Middleware/Mediaplan/environment_setup.sh
+++ Middleware/Mediaplan/environment_setup.sh
@@ -1,5 +1,13 @@
-sudo yum update
+sudo yum update -y
+
 sudo yum install openssh
-sudo yum install sshpass
-sudo yum install git-all
+
+wget http://sourceforge.net/projects/sshpass/files/latest/download -O sshpass.tar.gz
+tar -xvf sshpass.tar.gz
+cd sshpass-1.08
+sudo yum groupinstall "Development Tools"
+./configure
+sudo make install
+
+sudo yum install git-all -y
 git --version
