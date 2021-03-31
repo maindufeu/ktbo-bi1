46cad7ca97cfbd444e0e2f0d7ba6e3473ad861e1
edher <edher@ktbo.com>
2021-03-31 15:21:26 -0600

    ktbomco changes

--- Middleware/Mediaplan/ktbomco_mp.py
+++ Middleware/Mediaplan/ktbomco_mp.py
@@ -1,8 +1,3 @@
 import mp_checker as mp
-import datetime as dt
-import pandas as pd
 
-df = pd.read_excel('03302021_ktbo_kube_AdverityExample_Mediaplan_Abril-Miami.xlsx')
-
-mp.mp_validate(df)
-print(mp.mp_validate(df))
+mp.mp_validate()
