d34734749f0468ae419c857d88e7733ed38fd5f4
datakube2021 <kube@ktbo.com>
2021-03-31 22:18:23 +0000

    Update mediaplan_up.sh

--- Middleware/Mediaplan/mediaplan_up.sh
+++ Middleware/Mediaplan/mediaplan_up.sh
@@ -1,6 +1,5 @@
 sshpass -p Eemaa9eiF4aeteigheiyu3Mae0piej sftp ktbo@sftp.adverity.com -22 << !
-   cd uploads/mediaplan
-   lcd Documents/Work/KTBO/Mediaplans/sftp
+   lcd result
    mput *.xlsx
    bye
 !
