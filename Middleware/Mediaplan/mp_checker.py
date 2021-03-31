f5c8fd6281472c07b7da47cc9256833a2e499c77
edher <edher@ktbo.com>
2021-03-31 15:27:02 -0600

    version test

--- Middleware/Mediaplan/mp_checker.py
+++ Middleware/Mediaplan/mp_checker.py
@@ -44,6 +44,7 @@ def mp_validate():
                 print(f'El plan de medios {filename} tiene todas las columnas')
             else:
                 print('faltan columnas revise el archivo')
+                print('pito')
             if "2021" in filename:
                 filename = '2021' + (re.split('2021', i)[1])
             df['filename'] = filename
