b4346bd943121f0a565bcfa23370f49faeb39e68
edher <edher@ktbo.com>
2021-03-31 17:23:44 -0600

    shell

--- Middleware/Mediaplan/ktbomco_mp.py
+++ Middleware/Mediaplan/ktbomco_mp.py
@@ -5,6 +5,6 @@ if status == 0:
     print('0 exit')
     import subprocess
     print('sftp uploading...')
-    subprocess.call('chmod +x mediaplan_up.sh')
+    subprocess.call('chmod +x mediaplan_up.sh', shell=True)
     subprocess.call('./mediaplan_up.sh >> log.log', shell=True)
     print('succesful load')
