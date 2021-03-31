421d618913faf61fdb604e3136489840978ad0dc
edher <edher@ktbo.com>
2021-03-31 16:16:34 -0600

    sftp uploading

--- Middleware/Mediaplan/ktbomco_mp.py
+++ Middleware/Mediaplan/ktbomco_mp.py
@@ -3,3 +3,7 @@ import mp_checker as mp
 status = mp.mp_validate()
 if status == 0:
     print('0 exit')
+    import subprocess
+    print('sftp uploading...')
+    subprocess.call('mediaplan_up.sh')
+    print('succesful load')
