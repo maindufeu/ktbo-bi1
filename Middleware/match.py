d77378a8c9453a2102e0a9feb12cd76d8d668c20
edher <edher@ktbo.com>
2021-04-27 17:08:03 -0500

    list

--- Middleware/match.py
+++ Middleware/match.py
@@ -7,15 +7,25 @@ import re
 
 def mp_match(mediaplan, mediafacts):
     print('working on it')
-    mp_len = len(mediaplan)
-    mf_len = len(mediafacts)
-    campaigns_matched = mediaplan.join(mediafacts,  how = left)
-    matched_len = len(campaigns_matched)
-    matchrate = matched_len/mp_len
+    matched = 0
+    mp_u = mediaplan['Campaign Name'].unique()
+    mf_u = mediafacts['Campaign Name'].unique()
+    for i in mp_u:
+        if i not in mf_u:
+            print(f'falta {i} en los datastreams')
+        else:
+            matched = matched + 1
+
+    for i in mf_u:
+        if i not in mp_u:
+            print(f'falta {i} en los planes de medios')
+    mp_len = len(mp_u)
+    mf_len = len(mf_u)
+    matched_len = len(matched)
+    matchrate = matched/mp_len
     if valid_status == 0:
         print('el match rate es de:')
         print(matchrate)
     else:
         print('el mediaplan contiene errores')
     return campaigns_matched
-    print(matchrate)
