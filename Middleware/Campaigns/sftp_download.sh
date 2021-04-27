aadff48efc3e3a26b862d2ad1757164f09869603
edher <edher@ktbo.com>
2021-04-27 12:57:16 -0500

    sftp_download

---
+++ Middleware/Campaigns/sftp_download.sh
@@ -0,0 +1,65 @@
+sshpass -p Eemaa9eiF4aeteigheiyu3Mae0piej sftp ktbo@sftp.adverity.com -22 << !
+  cd uploads/test
+
+  lcd facebook
+  get facebook/*
+  lcd ..
+  rmdir facebook
+  mkdir facebook
+
+  lcd sizmekpr
+  get sizmekpr/*
+  lcd ..
+  rmdir sizmekpr
+  mkdir sizmekpr
+
+  lcd sizmekmx
+  get sizmekmx/*
+  rmdir sizmekmx
+  mkdir sizmekmx
+
+  lcd sizmekmi
+  get sizmekmi/*
+  lcd ..
+  rmdir sizmekmi
+  mkdir sizmekmi
+
+  lcd sizmekco
+  get sizmekco/*
+  lcd ..
+  rmdir sizmekco
+  mkdir sizmekco
+
+  lcd google
+  get google/*
+  lcd ..
+  rmdir google
+  mkdir google
+
+  lcd youtube
+  get youtube/*
+  lcd ..
+  rmdir youtube
+  mkdir youtube
+
+  lcd mp
+  get mp/*
+  lcd ..
+  rmdir mp
+  mkdir mp
+
+  lcd twitter
+  get twitter/*
+  lcd ..
+  rmdir twitter
+  mkdir twitter
+
+  lcd twitter
+  get othercampaigns/*
+  lcd ..
+  rmdir othercampaigns
+  mkdir othercampaigns
+
+
+  bye
+!
