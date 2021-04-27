697741f4143ccd2de37003dd4f77dc6f03b7754f
edher <edher@ktbo.com>
2021-04-27 16:22:23 -0500

    'mp_path

--- Middleware/matchrate.py
+++ Middleware/matchrate.py
@@ -6,7 +6,7 @@ import fnmatch
 
 path = 'Campaigns/**'
 pattern = '*.csv'
-mp_path = 'Campaigns/mp/'
+mp_path = 'Mediaplan/mp_processed'
 listfiles = []
 mpfiles = []
 frames = []
@@ -15,14 +15,16 @@ mp_frames =[]
 for filename in glob.iglob(path, recursive=True):
     if os.path.isfile(filename): # filter dirs
         if fnmatch.fnmatch(filename, pattern):
-            if fnmatch.fnmatch(filename, mp_pattern):
-                mpfiles.append(filename)
-                df = pd.read_csv(filename)
-                mp_frames .append(df)
-            else:
                 listfiles.append(filename)
                 df = pd.read_csv(filename)
                 frames.append(df)
+
+for filename in glob.iglob(mp_path, recursive=True):
+    if os.path.isfile(filename): # filter dirs
+        if fnmatch.fnmatch(filename, pattern):
+            mpfiles.append(filename)
+            df = pd.read_csv(filename)
+            mp_frames .append(df)
 
 mediafacts = pd.concat([df['Campaign', 'Datasource'] for df in frames], ignore_index=True)
 
