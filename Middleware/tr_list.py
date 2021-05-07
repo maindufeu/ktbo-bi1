179d10aad581dbd7fbb56a33b8a598f3a86aab03
edher <edher@ktbo.com>
2021-05-07 16:20:42 -0500

    git_push

--- Middleware/tr_list.py
+++ Middleware/tr_list.py
@@ -2,51 +2,50 @@ import requests
 import json
 from pprint import pprint
 
-def transform_l():
-    ###Authorization
-    url = "https://KTBO.datatap.adverity.com/api/transformer/"
+###Authorization
+url = "https://KTBO.datatap.adverity.com/api/transformer/"
 
-    payload={}
-    headers = {
-      'Authorization': 'Token c49e653ffa8a0c80768bbf1af0887905a56fff9b'
-    }
-    ###request
+payload={}
+headers = {
+  'Authorization': 'Token c49e653ffa8a0c80768bbf1af0887905a56fff9b'
+}
+###request
+response = requests.request("GET", url, headers=headers, data=payload).json()
+t_list = response['results']
+
+###loop to get all pages
+while response['next'] is not None:
+    url = response['next']
     response = requests.request("GET", url, headers=headers, data=payload).json()
-    t_list = response['results']
+    t_list.extend(response['results'])
+    #print(response['next'])
 
-    ###loop to get all pages
-    while response['next'] is not None:
-        url = response['next']
-        response = requests.request("GET", url, headers=headers, data=payload).json()
-        t_list.extend(response['results'])
-        #print(response['next'])
+#pprint(t_list)
 
-    #pprint(t_list)
+###loop to get all the instructions
+inst = {'transformations':[]}
+inst_d = {'name':[],'tr':[]}
 
-    ###loop to get all the instructions
-    inst = {'transformations':[]}
-    inst_d = {'name':[],'tr':[]}
+t_dict = {}
 
-    t_dict = {}
+cont = 1
+N = len(t_list)
 
-    cont = 1
-    N = len(t_list)
+for i in t_list:
+    url = f'https://KTBO.datatap.adverity.com/api/transformer/{i["id"]}'
+    response = requests.request("GET", url, headers=headers, data=payload).json()
+    #print(url)
+    transform2 = response['instructions']
+    key_name = response['name']
 
-    for i in t_list:
-        url = f'https://KTBO.datatap.adverity.com/api/transformer/{i["id"]}'
-        response = requests.request("GET", url, headers=headers, data=payload).json()
-        #print(url)
-        transform2 = response['instructions']
-        key_name = response['name']
+    print("%i/%i ---- %s"%(cont, N, key_name))
+    cont = cont + 1
 
-        print("%i/%i ---- %s"%(cont, N, key_name))
-        cont = cont + 1
-
-        #print(key_name, transform2)
-        t_dict[key_name] = transform2
+    #print(key_name, transform2)
+    t_dict[key_name] = transform2
 
 
-    with open('transformations.json', 'w') as fp:
-        json.dump(t_dict, fp)
-
-    return t_dict
+with open('transformations.json', 'w') as fp:
+    json.dump(t_dict, fp)
+    subprocess.call('git_push >> log_transformations.log', shell=True)
+    print("transformation updated")
