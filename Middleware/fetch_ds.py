2fc1641196dd91cfcf894c1c12e41eae0a72f883
edher <edher@ktbo.com>
2021-05-07 18:56:26 -0500

    fetch

---
+++ Middleware/fetch_ds.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+datastreams_id = [6,63,101,89,98,97,99]
+
+for i in datastreams_id:
+    url = f'https://KTBO.datatap.adverity.com/api/datastreams/{i}/fetch_fixed/'
+    payload = json.dumps({
+      "start": "2021-05-01T00:00:00Z",
+      "end": "2021-05-07T00:00:00Z"
+    })
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Token c49e653ffa8a0c80768bbf1af0887905a56fff9b'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload).json()
+    print(response)
