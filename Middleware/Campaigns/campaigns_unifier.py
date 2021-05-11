27c3bf29e674a8cf4a6cdaf893660eed457793ad
edher <edher@ktbo.com>
2021-05-11 13:59:45 -0500

    print different formats

--- Middleware/Campaigns/campaigns_unifier.py
+++ Middleware/Campaigns/campaigns_unifier.py
@@ -1,8 +1,20 @@
 import pandas as pd
 
+
 other = pd.read_excel("othercampaigns/acumulado_other.xlsx")
 twitter = pd.read_excel("twitter/acumulado_twitter.xlsx")
+twitter = twitter['Campaign'].upper().strip(" ")
+print("Twitter length:")
+print(len(twitter))
 
-print(other)
+print("othercampaigns")
+other = other['Campaign'].upper().strip(" ")
+print("Other Campaigns length:")
+print(len(other))
 
-print(twitter)
+
+google = pd.read_csv("google\409571_adwords_adwords-143-20210427-94eedaa01640429a86addf575c77449a.csv")
+google = google['Campaign_duplicate']
+print(len(google))
+c_unified =twitter + other +google
+print(len(c_unified))
