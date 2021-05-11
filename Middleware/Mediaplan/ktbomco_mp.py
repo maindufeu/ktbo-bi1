46e44b42fd3de0d13e2fb698bc00fcd519af4ad5
edher <edher@ktbo.com>
2021-05-11 18:06:44 -0500

    matchrate_upd.sh

--- Middleware/Mediaplan/ktbomco_mp.py
+++ Middleware/Mediaplan/ktbomco_mp.py
@@ -4,6 +4,9 @@ status = mp.mp_validate()
 if status == 0:
     print('0 exit')
     import subprocess
+    print('---matchrate evaluation:')
+    subprocess.call('chmod +x mediaplan_up.sh', shell=True)
+    subprocess.call('matchrate_upd.sh', shell=True)
     print('sftp uploading...')
     subprocess.call('chmod +x mediaplan_up.sh', shell=True)
     subprocess.call('./mediaplan_up.sh >> log.log', shell=True)
