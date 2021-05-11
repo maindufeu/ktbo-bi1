f8e1af89f52e887b306e35f24b921f0f666ca7d9
edher <edher@ktbo.com>
2021-05-10 21:06:00 -0500

    fetch date

--- Middleware/fetch_ds.py
+++ Middleware/fetch_ds.py
@@ -6,8 +6,8 @@ datastreams_id = [6,63,101,89,98,97,99]
 for i in datastreams_id:
     url = f'https://KTBO.datatap.adverity.com/api/datastreams/{i}/fetch_fixed/'
     payload = json.dumps({
-      "start": "2021-05-01T00:00:00Z",
-      "end": "2021-05-07T00:00:00Z"
+      "start": "2021-01-01T00:00:00Z",
+      "end": "2021-05-10T00:00:00Z"
     })
     headers = {
       'Content-Type': 'application/json',
