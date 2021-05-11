726bf3edbad140f2dcfe61ba0832c0648f795a98
edher <edher@ktbo.com>
2021-05-11 15:05:07 -0500

    print columns

--- Middleware/Campaigns/campaigns_unifier.py
+++ Middleware/Campaigns/campaigns_unifier.py
@@ -1,26 +1,49 @@
 import pandas as pd
+import glob
+import os
+import fnmatch
 
 
-other = pd.read_excel("othercampaigns/acumulado_other.xlsx")
-twitter = pd.read_excel("twitter/acumulado_twitter.xlsx")
+
+other = pd.read_excel("othercampaigns/acumulado_other.xlsx", usecols = ['Campaign'])
+twitter = pd.read_excel("twitter/acumulado_twitter.xlsx", usecols = ['Campaign'])
 twitter = twitter['Campaign'].str.upper().str.strip(" ").unique().tolist()
-
 print("Twitter length:")
 print(len(twitter))
 
-print("othercampaigns")
 other = other['Campaign'].str.upper().str.strip(" ").unique().tolist()
 print("Other Campaigns length:")
 print(len(other))
 
+path = 'google/*'
+pattern = '*.csv'
 
-google = pd.read_csv("google/409571_adwords_adwords-143-20210427-94eedaa01640429a86addf575c77449a.csv")
-google = google['Campaign_duplicate'].str.upper().str.strip(" ").unique().tolist()
-print("Googleleght:")
-print(len(google))
+########
+path = 'Campaigns/**'
+pattern = '*.csv'
+mp_path = 'Mediaplan/mp_processed'
+listfiles = []
+mpfiles = []
+frames = []
+mp_frames =[]
 
-c_unified =twitter + other +google
-print("unifiedListLength:")
-print(len(other))
+for filename in glob.iglob(path, recursive=True):
+    if os.path.isfile(filename): # filter dirs
+        if fnmatch.fnmatch(filename, pattern):
+            print(filename)
+            df = pd.read_csv(filename)
+            print(df.columns)
+######
 
-print(len(c_unified))
+#for filename in glob.iglob(path, recursive=True):
+#    if os.path.isfile(filename): # filter dirs
+#        if fnmatch.fnmatch(filename, pattern):
+#            google = pd.read_csv('google/{i}', usecols=['Campaign_duplicate'])
+#            google = google.unique().tolist()
+#            print("Googleleght:")
+#            print(len(google))
+
+#facebook = pd.read_csv("google/409571_adwords_adwords-143-20210427-94eedaa01640429a86addf575c77449a.csv")
+
+
+#print(len(c_unified))
