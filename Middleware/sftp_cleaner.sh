3b095c79be5ebcc54b8a7fe3700370decbb41ac0
edher <edher@ktbo.com>
2021-05-11 17:51:18 -0500

    s

--- Middleware/sftp_cleaner.sh
+++ Middleware/sftp_cleaner.sh
@@ -2,5 +2,10 @@ sshpass -p Eemaa9eiF4aeteigheiyu3Mae0piej sftp ktbo@sftp.adverity.com -22 << !
   cd uploads/
 
   rm test/facebook/*
+  rm test/sizmekco/*
+  rm test/sizmekmi/*
+  rm test/sizmekmx/*
+  rm test/sizmekpr/*
+  rm test/google/*
   bye
 !
