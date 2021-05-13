0964f311deb33b64bd7ca81ae392cb215b795db5
edher <edher@ktbo.com>
2021-05-13 17:05:33 -0500

    a

--- Middleware/match.py
+++ Middleware/match.py
@@ -15,7 +15,7 @@ def mp_match(mediaplan, mediafacts):
     mf_u = mediafacts['Campaign Name'].unique()
     for i in mp_u:
         if i not in mf_u:
-            mediaplan_c = mediaplan[mediaplan['Campaign Name'] == i]['Mediaplan'][0]
+            mediaplan_c = list(mediaplan[mediaplan['Campaign Name'] == i]['Mediaplan'])[0]
             print(f'falta {i} del plan de medios {mediaplan_c} en los datastreams')
             mp_list.append(mediaplan_c)
             campaigns_matched.append(i)
